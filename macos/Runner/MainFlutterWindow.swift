bc0c5e1bdc9e7fbfc7540b58c4c2a906d52e2d77
kobayashikantaro <kobayashikantaro@me.com>
2026-07-21 11:31:05 +0900

    v0.05 Configure macOS app window and name

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,11 +4,32 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
 
-    RegisterGeneratedPlugins(registry: flutterViewController)
+    self.contentViewController = flutterViewController
+
+    // ウィンドウタイトル
+    self.title = "SIGNAL FLOW Show Time"
+
+    // 最小ウィンドウサイズ
+    self.minSize = NSSize(
+      width: 520,
+      height: 500
+    )
+
+    // 起動時のウィンドウサイズ
+    self.setContentSize(
+      NSSize(
+        width: 900,
+        height: 650
+      )
+    )
+
+    // 起動時に画面中央へ配置
+    self.center()
+
+    RegisterGeneratedPlugins(
+      registry: flutterViewController
+    )
 
     super.awakeFromNib()
   }
